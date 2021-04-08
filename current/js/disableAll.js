6b0fa5f433e7628a9738e73b82b80a008d5481fd
Daniel Unterholzner <pianojammer@comcast.net>
2021-04-08 12:54:43 -0700

    Adding customizeable files

---
+++ current/js/disableAll.js
@@ -0,0 +1,26 @@
+games.custom.title = 'Insurgence';
+games.custom.locations = [];
+
+setTimeout(function() {
+    var head = document.querySelector('div#gameMenu div.menu');
+
+    document.querySelectorAll('div#gameMenu div.menu .item').forEach((val, key, parent) => {
+        if (!val.classList.contains('active')) {
+            head.removeChild(val);
+        }
+    });
+
+    $('div.button').click = null;
+    var inputs = $('input');
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].id !== 'dark-theme') {
+            inputs[i].disabled = true;
+        }
+    }
+
+    $('input.search').unbind('click');
+    $('input[name=status]').parent().unbind('click');
+    $('button').prop('disabled', true);
+    $('label#loadData').unbind('click');
+}, 100);
