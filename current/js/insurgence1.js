25e898be0894243ca355f84411ae3537a848f269
Daniel Unterholzner <pianojammer@comcast.net>
2021-04-08 20:11:37 -0700

    Added a guide to the js controlling the current nuzlocke data to remind me of Pokemon entry later

--- current/js/insurgence1.js
+++ current/js/insurgence1.js
@@ -1,1 +1,9 @@
 localStorage.setItem('selectedGame', 'custom');
+
+/* Pokemon Entry:
+-----------------
+customX-name: Pokemon Species
+customX-encounter: Pokemon Species
+customX-nickname: Pokemon Nickname
+customX-status: captured | received | traded | missed | stored | deceased
+*/
