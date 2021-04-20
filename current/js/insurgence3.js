1bcaeed3d5ea90d767b63e124767b83d942a09ec
Daniel Unterholzner <pianojammer@comcast.net>
2021-04-20 12:51:09 -0700

    Adding Shade Forest pokemon

--- current/js/insurgence3.js
+++ current/js/insurgence3.js
@@ -12,3 +12,8 @@ localStorage.setItem("custom0-name", "Delta Squirtle");
 localStorage.setItem("custom0-encounter", "delta-squirtle");
 localStorage.setItem("custom0-nickname", "Fury");
 localStorage.setItem("custom0-status", "received");
+
+localStorage.setItem("custom1-name", "Ghastly");
+localStorage.setItem("custom1-encounter", "ghastly");
+localStorage.setItem("custom1-nickname", "");
+localStorage.setItem("custom1-status", "deceased");
